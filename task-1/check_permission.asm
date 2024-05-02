3dde826c845843fec2a0b1aa893b537aabd17816
alexandraontica <alexandra.ontica@gmail.com>
2024-05-02 18:23:49 +0300

    incercare idioata

--- task-1/check_permission.asm
+++ task-1/check_permission.asm
@@ -19,6 +19,22 @@ check_permission:
    
     ;; Your code starts here
 
+    mov ecx, 16777215 ; 00000000111111111111111111111111 decimal
+    and ecx, eax ; the id becomes 0
+
+    mov edx, 16777215
+    cmp edx, ecx
+    jz has_all_permissions
+    jmp doesnt_have_all_permissions
+
+has_all_permissions:
+    mov ebx, 1
+    jmp end_function
+
+doesnt_have_all_permissions:
+    mov ebx, 0
+
+end_function:
     ;; Your code ends here
     
     ;; DO NOT MODIFY
