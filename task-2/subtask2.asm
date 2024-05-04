b77f57bd12b7913f5ee2bcc6be3fe84b4b8fb0f2
alexandraontica <alexandra.ontica@gmail.com>
2024-05-04 18:46:25 +0300

    am incercat si eu ceva

--- task-2/subtask2.asm
+++ task-2/subtask2.asm
@@ -13,10 +13,91 @@ check_passkeys:
 
     mov ebx, [ebp + 8]      ; requests
     mov ecx, [ebp + 12]     ; length
-    mov eax, [ebp + 16]     ;
+    mov eax, [ebp + 16]     ; connected
     ;; DO NOT MODIFY
 
     ;; Your code starts here
+
+    mov esi, 0
+init:
+    cmp esi, ecx
+    jge end_init
+
+    mov byte [eax + esi], 0
+
+    inc esi
+    jmp init
+
+end_init:
+    mov esi, 0
+verif:
+    cmp esi, ecx
+    jge end_init
+
+    mov di, word [ebx + esi + 2] ; save the current passkey
+    shl esi, 16
+    
+    mov dx, 0x8001 ; first and last bit is set
+    and dx, di
+    cmp dx, 0x8001
+    jne end_if1
+
+    mov dx, di ; temp
+    shl edi, 16 ; I want the di register free, but i do not want to lose the data in it
+    mov di, 0
+    shr dx, 1
+    and dx, 0x7F
+    mov si, 0
+loop1:
+    cmp si, 7
+    jge end_loop1
+
+    test dx, 1
+    jz isnt_set1
+
+    inc di
+isnt_set1:
+    shl di, 1
+    inc si
+    jmp loop1
+
+end_loop1:
+    test di, 1
+    jnz end_if1
+
+    shr edi, 16
+    mov dx, di ; temp
+    shl edi, 16 ; I want the di register free, but i do not want to lose the data in it
+    mov di, 0
+    shr dx, 8
+    and dx, 0x7F
+    mov si, 0
+loop2:
+    cmp si, 7
+    jge end_loop1
+
+    test dx, 1
+    jz isnt_set2
+
+    inc di
+isnt_set2:
+    shl di, 1
+    inc si
+    jmp loop2
+end_loop2:
+    test di, 1
+    jz end_if1
+
+    mov edi, esi
+    shr edi, 16
+    mov byte [eax + edi], 1
+
+end_if1:
+    shr esi, 16
+    inc esi
+    jmp end_verif
+
+end_verif:
 
     ;; Your code ends here
 
