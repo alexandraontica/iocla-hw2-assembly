e5866aee472eb2aa5f5980fd98aca5fcc4273cb1
alexandraontica <alexandra.ontica@gmail.com>
2024-05-05 13:54:11 +0300

    work in progress

--- task-2/subtask1.asm
+++ task-2/subtask1.asm
@@ -52,8 +52,45 @@ sort_loop2:
     imul ecx, ecx, 55 ; multiply the index by the size of the structure
     add ebx, ecx ; ebx points to the curent request
 
-    
+    ; check the admin fields:
+    mov cl, byte [ebx] ; admin from the second loop
+    mov al, byte [edx] ; admin from the first loop
+    and cl, 1
+    and al, 1
+    cmp cl, al 
+    je admins_are_eq
+    jl next_request
 
+    ; the request in ebx in made by an admin,
+    ; the one in edx is not
+    ; swap:
+
+    mov eax, edx 
+    mov edx, ebx
+    mov ebx, eax
+    jmp next_request
+
+admins_are_eq:
+    ; check the prio fields:
+    mov cl, byte [ebx + 1] ; prio from the second loop
+    mov al, byte [edx + 1] ; prio from the first loop
+    cmp al, cl 
+    je prios_are_eq
+    jl next_request
+
+    ; swap:
+
+    mov eax, edx 
+    mov edx, ebx
+    mov ebx, eax
+    jmp next_request
+
+prios_are_eq:
+    ; check the usernames:
+    mov ecx, [ebx + 4] ; username from the second loop
+    mov eax, [edx + 4] ; username from the first loop
+
+next_request:
     inc edi 
     jmp sort_loop2
 end_sort_loop2:
