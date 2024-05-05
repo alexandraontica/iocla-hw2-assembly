904c8fe1312b8d88f7c0cdf76e4c926b7ff146af
alexandraontica <alexandra.ontica@gmail.com>
2024-05-05 12:29:46 +0300

    am inceput si sortarea

--- task-2/subtask1.asm
+++ task-2/subtask1.asm
@@ -1,39 +1,70 @@
-; %include "../include/io.mac"
+%include "../include/io.mac"
 
-; ; declare your structs here
+; declare your structs here
 
-; ; struc creds
-; ;     passkey: resw 1
-; ;     username: resb 51
-; ; endstruc
+; struc creds
+;     passkey: resw 1
+;     username: resb 51
+; endstruc
 
-; ; struc request 
-; ;     admin: resb 1
-; ;     prio: resb 1
-; ;     login_creds: resb 53  ; 2 bytes (short) + 51 bytes (string)
-; ; endstruc
+; struc request 
+;     admin: resb 1
+;     prio: resb 1
+;     login_creds: resb 53  ; 2 bytes (short) + 51 bytes (string)
+; endstruc
 
-; section .text
-;     global sort_requests
-;     extern printf
+section .text
+    global sort_requests
+    extern printf
 
-; sort_requests:
-;     ;; DO NOT MODIFY
-;     enter 0,0
-;     pusha
+sort_requests:
+    ;; DO NOT MODIFY
+    enter 0,0
+    pusha
 
-;     mov ebx, [ebp + 8]      ; requests
-;     mov ecx, [ebp + 12]     ; length
-;     ;; DO NOT MODIFY
+    mov ebx, [ebp + 8]      ; requests
+    mov ecx, [ebp + 12]     ; length
+    ;; DO NOT MODIFY
 
-;     ;; Your code starts here
+    ;; Your code starts here
 
+    xor esi, esi
+sort_loop1:
+    mov ecx, [ebp + 12] 
+    dec ecx 
+    cmp esi, ecx 
+    jge end_sort_loop1
 
+    mov edx, [ebp + 8] ; requests array address
+    mov ecx, esi ; the index of the element in the array
+    imul ecx, ecx, 55 ; multiply the index by the size of the structure
+    add edx, ecx ; edx points to the curent request
+
+    inc esi
+    mov edi, esi
+    dec esi
+sort_loop2:
+    cmp edi, [ebp + 12] 
+    jge end_sort_loop2
+
+    mov ebx, [ebp + 8] ; requests array address
+    mov ecx, edi ; the index of the element in the array
+    imul ecx, ecx, 55 ; multiply the index by the size of the structure
+    add ebx, ecx ; ebx points to the curent request
+
+    
+
+    inc edi 
+    jmp sort_loop2
+end_sort_loop2:
+    inc esi
+    jmp sort_loop1
+end_sort_loop1:
   
-;     ;; Your code ends here
+    ;; Your code ends here
 
-;     ;; DO NOT MODIFY
-;     popa
-;     leave
-;     ret
-;     ; DO NOT MODIFY
+    ;; DO NOT MODIFY
+    popa
+    leave
+    ret
+    ; DO NOT MODIFY
--- task-2/subtask2.asm
+++ task-2/subtask2.asm
@@ -62,7 +62,7 @@ end_loop1:
 
     mov dx, di ; temp
     shr dx, 8
-    and dx, 0x7F
+    and dx, 0x7F ; ; 0xF6 = 0b1111111
     xor ecx, ecx ; index
     xor ebx, ebx ; counter
 loop2:
@@ -89,73 +89,7 @@ end_if1:
     jmp init
 
 end_init:
-    ; xor esi, esi
-; verif:  ;; asta nu face decat o iteratie, de ce nu stiu
-;     cmp esi, [ebp + 12]
-;     jge end_verif
-
-;     ; mov edx, [ebp + 8] ; requests array address
-;     ; mov ecx, esi ; the index of the element in the array
-;     ; imul ecx, ecx, 55 ; multiply the index by the size of the structure
-;     ; add edx, ecx ; edx points to the curent request
-;     ; add edx, 2 ; edx points to the passkey in the request
-;     ; mov di, word [edx] ; save the passkey
- 
-;     ; mov dx, 0x8001 ; first and last bit are set
-;     ; and dx, di
-;     ; cmp dx, 0x8001
-;     ; jne end_if1
-
-;     ; mov dx, di ; temp
-;     ; shr dx, 1
-;     ; and dx, 0x7F
-;     ; xor ecx, ecx ; index
-;     ; xor ebx, ebx ; counter
-; ; loop1:
-; ;     cmp ecx, 7
-; ;     jge end_loop1
-
-; ;     test dx, 1
-; ;     jz isnt_set1
-
-; ;     inc ebx
-; ; isnt_set1:
-; ;     shl dx, 1
-; ;     inc ecx
-; ;     jmp loop1
-
-; ; end_loop1:
-; ;     test ebx, 1
-; ;     jnz end_if1
-
-; ;     mov dx, di ; temp
-; ;     shr dx, 8
-; ;     and dx, 0x7F
-; ;     xor ecx, ecx ; index
-; ;     xor ebx, ebx ; counter
-; ; loop2:
-; ;     cmp ecx, 7
-; ;     jge end_loop2
-
-; ;     test dx, 1
-; ;     jz isnt_set2
-
-; ;     inc ebx
-; ; isnt_set2:
-; ;     shl dx, 1
-; ;     inc ecx
-; ;     jmp loop2
-; ; end_loop2:
-; ;     test ebx, 1
-; ;     jz end_if1
-
-;     mov byte [eax + esi], 1
-
-; end_if1:
-;     inc esi
-;     jmp end_verif
-
-; end_verif:
+    
     ;; Your code ends here
 
     ;; DO NOT MODIFY
