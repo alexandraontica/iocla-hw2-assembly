2ad62fe06db47ea608c530f37380b585934d6427
alexandraontica <alexandra.ontica@gmail.com>
2024-05-06 01:33:47 +0300

    progress

--- task-4/labyrinth.asm
+++ task-4/labyrinth.asm
@@ -1,48 +1,118 @@
-; %include "../include/io.mac"
+%include "../include/io.mac"
 
-; extern printf
-; extern position
-; global solve_labyrinth
+extern printf
+extern position
+global solve_labyrinth
 
-; ; you can declare any helper variables in .data or .bss
+; you can declare any helper variables in .data or .bss
 
-; section .bss
-;     row resb 30  ; assume maximum 30 columns
-;     m resd 1
-;     n resd 1
+section .bss
+    m resd 1
+    n resd 1
+    var resd 1
 
-; section .text
+section .text
 
-; ; void solve_labyrinth(int *out_line, int *out_col, int m, int n, char **labyrinth);
-; solve_labyrinth:
-;     ;; DO NOT MODIFY
-;     push    ebp
-;     mov     ebp, esp
-;     pusha
+; void solve_labyrinth(int *out_line, int *out_col, int m, int n, char **labyrinth);
+solve_labyrinth:
+    ;; DO NOT MODIFY
+    push    ebp
+    mov     ebp, esp
+    pusha
 
-;     mov     eax, [ebp + 8]  ; unsigned int *out_line, pointer to structure containing exit position
-;     mov     ebx, [ebp + 12] ; unsigned int *out_col, pointer to structure containing exit position
-;     mov     ecx, [ebp + 16] ; unsigned int m, number of lines in the labyrinth
-;     mov     edx, [ebp + 20] ; unsigned int n, number of colons in the labyrinth
-;     mov     esi, [ebp + 24] ; char **a, matrix represantation of the labyrinth
-;     ;; DO NOT MODIFY
+    mov     eax, [ebp + 8]  ; unsigned int *out_line, pointer to structure containing exit position
+    mov     ebx, [ebp + 12] ; unsigned int *out_col, pointer to structure containing exit position
+    mov     ecx, [ebp + 16] ; unsigned int m, number of lines in the labyrinth
+    mov     edx, [ebp + 20] ; unsigned int n, number of colons in the labyrinth
+    mov     esi, [ebp + 24] ; char **a, matrix represantation of the labyrinth
+    ;; DO NOT MODIFY
    
-;     ;; Freestyle starts here
+    ;; Freestyle starts here
     
-;     mov byte [eax], 0
-;     mov byte [ebx], 0
-;     mov dword [m], ecx
-;     mov dword [n], edx
+    mov dword [eax], 0
+    mov dword [ebx], 0
+    mov dword [m], ecx
+    mov dword [n], edx
 
-; labyrinth_loop:
-;     mov ecx, 
+labyrinth_loop:
+    ; set current element to '1'
+    mov edi, dword [eax]
+    mov ecx, [esi + edi * 4] ; current row in matrix, *4 bc that is the size of a pointer
+    mov edi, dword [ebx]
+    mov byte [ecx + edi], '1'
 
-;     ;; Freestyle ends here
-; end:
-;     ;; DO NOT MODIFY
-
-;     popa
-;     leave
-;     ret
+    mov edx, [m]
+    dec edx
+    cmp [eax], edx
+    jge end
     
-;     ;; DO NOT MODIFY
+    ; check the element below
+    mov edi, dword [eax]
+    inc edi
+    mov ecx, [esi + edi * 4]
+    mov edi, dword [ebx]
+    cmp byte [ecx + edi], '0'
+    jne next_if1
+    
+    inc dword [eax]
+
+next_if1:
+    cmp dword [eax], 0
+    jle next_if2
+
+    ; check the element above
+    mov edi, dword [eax]
+    dec edi
+    mov ecx, [esi + edi * 4]
+    mov edi, dword [ebx]
+    cmp byte [ecx + edi], '0'
+    jne next_if2
+
+    dec dword [eax]
+
+next_if2:
+    mov edx, [n]
+    dec edx
+    cmp [ebx], edx
+    jge end
+
+    mov edi, dword [eax]
+    mov ecx, [esi + edi * 4]
+    mov edi, dword [ebx]
+    inc edi
+    cmp byte [ecx + edi], '0'
+    jne next_if3
+
+    inc dword [ebx]
+
+next_if3:
+    cmp dword [ebx], 0
+    jle cond
+
+    ; check the element above
+    mov edi, dword [eax]
+    mov ecx, [esi + edi * 4]
+    mov edi, dword [ebx]
+    dec edi
+    cmp byte [ecx + edi], '0'
+    jne cond
+
+    dec dword [ebx]
+
+cond:
+    mov edi, dword [eax]
+    mov ecx, [esi + edi * 4] ; current row in matrix, *4 bc that is the size of a pointer
+    mov edi, dword [ebx]
+    cmp byte [ecx + edi], '0'
+    jne end
+    jmp labyrinth_loop
+
+    ;; Freestyle ends here
+end:
+    ;; DO NOT MODIFY
+
+    popa
+    leave
+    ret
+    
+    ;; DO NOT MODIFY
